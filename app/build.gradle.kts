096229f5fa72a4ae99a0820c87f5a290eee877f7
Ensar Sarajčić <dev@ensarsarajcic.com>
2024-01-22 10:41:00 +0100

    Add simple GitHub action for static checks and tests
    Enabled android lint, added detekt and created github workflow for PRs.
    Related to discussion: https://github.com/orgs/FossifyOrg/discussions/83

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.android)
     alias(libs.plugins.kotlinAndroid)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.detekt)
     base
 }
 
@@ -90,7 +91,8 @@ android {
 
     lint {
         checkReleaseBuilds = false
-        abortOnError = false
+        abortOnError = true
+        warningsAsErrors = true
     }
 }
 
