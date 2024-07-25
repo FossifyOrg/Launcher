6401e861683564001885df27a0b5726ea8d3ebf6
Naveen Singh <snaveen935@gmail.com>
2024-07-25 14:11:52 +0530

    Rename `fdroid` flavor to `foss`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
     flavorDimensions.add("variants")
     productFlavors {
         register("core")
-        register("fdroid")
+        register("foss")
         register("prepaid")
     }
 
